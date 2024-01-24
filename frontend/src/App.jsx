769ff9cf23c908ea24f52aa5729739f7b7e983eb
Your Name <you@example.com>
2024-01-24 14:28:46 +0300

    headerandfooter

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Policy from './components/Policy'
+
+function App() {
+  
+
+  return (
+    <>
+    <Header></Header>
+    <h1>hello</h1>
+    <Policy></Policy>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default App
