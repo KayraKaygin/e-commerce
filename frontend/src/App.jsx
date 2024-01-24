e0f40de34570d0f67636f3f3b81a11f4e90a957c
Your Name <you@example.com>
2024-01-24 22:33:44 +0300

    Products&Categories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
+import Categories from './components/Layout/Categories/Categories'
 import Footer from './components/Layout/Footer/Footer'
 import Header from './components/Layout/Header/Header'
 import Policy from './components/Layout/Policy/Policy'
+import Products from './components/Layout/Products/Products'
 import Slider from './components/Layout/Slider/Slider'
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
     <>
     <Header></Header>
     <Slider></Slider>
+    <Categories></Categories>
+    <Products></Products>
     <Policy></Policy>
     <Footer></Footer>
     </>
